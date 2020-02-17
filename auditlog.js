c57623b890107a2321666e79c46541fb0d1e9074
Sundeus Technology <51749897+sundeus@users.noreply.github.com>
2020-02-17 10:09:27 +0530

    html
    Demo

---
+++ auditlog.js
@@ -0,0 +1,11 @@
+//   function closeForm() {
+//    document.getElementById("myModal").style.display = "none";
+//  }
+$(document).ready(function(){
+$('#MybtnModelPreventScript').click(function(){
+$('#MymodelPreventScript').modal({
+  backdrop: 'static',
+  // keyboard: false
+});
+});
+});
